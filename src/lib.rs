da6c59b35cffc36f04e44db0f060a6b878be44f6
10maurycy10 <10maurycy10@gmail.com>
2022-02-10 15:14:53 -0800

    Add mutable support

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 fn helper<'a, 'b, T>(_: &'a &'b (), v: &'b T) -> &'a T { v }
 
+
+fn helper_mut<'a, 'b, T>(_: &'a &'b (), v: &'b mut T) -> &'a mut T { v }
+
 #[cfg(not(debug_assertions))]
 compile_error!("Don't deploy this shit to production you madman");
 
@@ -7,5 +10,12 @@ compile_error!("Don't deploy this shit to production you madman");
 /// May introduce undefined behavior.
 pub fn make_static<'a, T>(input: &'a T) -> &'static T {
     let f: fn(_, &'a T) -> &'static T = helper;
+    f(&&(), input)
+}
+
+/// Use black magic fuckery to change the lifetime on a mutable refrence
+/// May introduce undefined behavior.
+pub fn make_static_mut<'a, 'b, T>(input: &'a mut T) -> &'b static T {
+    let f: fn(_, &'a mut T) -> &'b static T = helper_mut;
     f(&&(), input)
 }
